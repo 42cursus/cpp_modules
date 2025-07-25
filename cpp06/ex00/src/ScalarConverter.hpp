d58fbb1e7b29fbfd5203c0c4189e4a4e5186ebff
Andrei Belov <abelov@student.42london.com>
2025-07-25 22:00:00 +0100

    cpp06: boilerplate
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

---
+++ cpp06/ex00/src/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 21:43:03 by abelov            #+#    #+#             */
+/*   Updated: 2025/07/25 21:43:13 by abelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+
+class ScalarConverter
+{
+
+};
+
+
+#endif //SCALARCONVERTER_HPP
