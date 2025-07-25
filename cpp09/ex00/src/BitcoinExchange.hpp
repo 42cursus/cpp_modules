d4138db1a42423cc9ea723e0b529ff3655163bd1
Andrei Belov <abelov@student.42london.com>
2025-07-25 23:33:44 +0100

    cpp09: boilerplate
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

---
+++ cpp09/ex00/src/BitcoinExchange.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 23:26:33 by abelov            #+#    #+#             */
+/*   Updated: 2025/07/25 23:26:33 by abelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+
+class BitcoinExchange
+{
+
+};
+
+
+#endif //BITCOINEXCHANGE_HPP
