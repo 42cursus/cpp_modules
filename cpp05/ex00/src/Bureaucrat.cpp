bd63ab34fa3469289f84d0b3ed46dd07a841f502
Andrei Belov <abelov@student.42london.com>
2026-01-11 20:37:23 +0000

    cpp05: ex00

--- cpp05/ex00/src/Bureaucrat.cpp
+++ cpp05/ex00/src/Bureaucrat.cpp
@@ -10,4 +10,143 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sstream>
 #include "Bureaucrat.hpp"
+
+/*
+** -------------------------------- STATIC VARS -------------------------------
+*/
+
+const int           Bureaucrat::GRADE_RANGE_MAX = 1;
+const int           Bureaucrat::GRADE_RANGE_MIN = 150;
+const               std::string& Bureaucrat::DEFAULT_NAME = "default";
+
+const std::string   Bureaucrat::GradeTooHighException::_msg = Bureaucrat::GradeTooHighException::build_msg();
+const std::string   Bureaucrat::GradeTooLowException::_msg = Bureaucrat::GradeTooLowException::build_msg();
+
+/*
+** ------------------------------- CONSTRUCTORS -------------------------------
+*/
+
+Bureaucrat::Bureaucrat() : _name(DEFAULT_NAME), _grade(GRADE_RANGE_MIN) {}
+
+Bureaucrat::Bureaucrat(const std::string& name, const int grade) : _name(name), _grade(grade) {
+    if (_grade < GRADE_RANGE_MAX)
+        throw GradeTooHighException();
+    if (_grade > GRADE_RANGE_MIN)
+        throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException() : GradeException() {}
+Bureaucrat::GradeTooLowException::GradeTooLowException() : GradeException()  {}
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+void Bureaucrat::incrementGrade(const int inc) {
+
+    int target = _grade - inc;
+    if (target <= GRADE_RANGE_MAX)
+        throw GradeTooHighException();
+    _grade = target;
+}
+
+void  Bureaucrat::incrementGrade()
+{
+    incrementGrade(1);
+}
+
+void Bureaucrat::decrementGrade(const int dec)
+{
+    int target = _grade + dec;
+    if (target >= GRADE_RANGE_MIN)
+        throw GradeTooLowException();
+    _grade = target;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    decrementGrade(1);
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
+    if (this != &other) {
+        _name = other._name;
+        _grade = other._grade;
+    }
+    return *this;
+}
+
+/*
+** ------------------------------- DESTRUCTORS ---------------------------------
+*/
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string Bureaucrat::getName() const {
+    return _name;
+}
+
+int Bureaucrat::getGrade() const {
+    return _grade;
+}
+
+/*
+** -------------------------------- OVERLOADS ---------------------------------
+*/
+
+std::ostream& operator<<(std::ostream& oss, const Bureaucrat& bureaucrat)
+{
+    oss << bureaucrat.getName();
+    oss << ", bureaucrat grade ";
+    oss << bureaucrat.getGrade();
+    oss << ".\n";
+    return oss;
+}
+
+/*
+** -------------------------------- ACCESSORS ---------------------------------
+*/
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return _msg.c_str();
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return _msg.c_str();
+}
+
+/*
+** ---------------------------------- OTHER -----------------------------------
+*/
+
+
+
+std::string Bureaucrat::GradeTooLowException::build_msg() {
+    std::ostringstream oss;
+    oss << FT_RED
+        << "The grade is too low. Valid range is ["
+        << Bureaucrat::GRADE_RANGE_MAX
+        << ".."
+        << Bureaucrat::GRADE_RANGE_MIN
+        << "]."
+        << FT_RST;
+    return oss.str();
+}
+
+std::string Bureaucrat::GradeTooHighException::build_msg()
+{
+    std::ostringstream oss;
+    oss << FT_RED
+        << "The grade is too high. Valid range is ["
+        << Bureaucrat::GRADE_RANGE_MAX
+        << ".."
+        << Bureaucrat::GRADE_RANGE_MIN
+        << "]."
+        << FT_RST;
+    return oss.str();
+}
--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -10,4 +10,88 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Bureaucrat.hpp"
 
+int main(){
+    try {
+        Bureaucrat Bobby("Bobby", 30);
+        Bureaucrat Mandy("Mandy", 1);
+        Bureaucrat Tonny("Tonny", 150);
+
+        try {
+            Bureaucrat Victoria("Victoria", 0);
+        } catch (Bureaucrat::GradeTooHighException &tooHight) {
+            std::cout << "Error: " << tooHight.what() << std::endl;
+        } catch (Bureaucrat::GradeTooLowException &tooLow) {
+            std::cout << "Error: " << tooLow.what() << std::endl;
+        }
+
+        try {
+            Bureaucrat Leon("Leon", 151);
+        } catch (Bureaucrat::GradeException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+
+        std::cout << std::endl;
+        std::cout << FT_UND "Bureaucrats:" FT_RST << std::endl;
+        std::cout << Bobby;
+        std::cout << Mandy;
+        std::cout << Tonny;
+        std::cout << std::endl;
+
+        std::cout << FT_UND "Decrementing Grades\n" FT_RST << std::endl;
+        std::cout << "Demoting Bobby's grade by 4:" << std::endl;
+        try {
+            Bobby.decrementGrade(4);
+        } catch (Bureaucrat::GradeTooLowException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+        std::cout << Bobby << std::endl;
+
+        std::cout << "Demoting Mandy's grade by 3:" << std::endl;
+        try {
+            Mandy.decrementGrade(3);
+        } catch (Bureaucrat::GradeTooLowException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+        std::cout << Mandy << std::endl;
+
+        std::cout << "Demoting Tonny's grade by 14:" << std::endl;
+        try {
+            Tonny.decrementGrade(14);
+        } catch (Bureaucrat::GradeTooLowException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+        std::cout << Tonny << std::endl;
+
+        std::cout << FT_UND "\nIncrementting Grades\n" FT_RST << std::endl;
+        std::cout << "Promoting Bobby's grade by 14:" << std::endl;
+        try {
+            Bobby.incrementGrade(14);
+        } catch (Bureaucrat::GradeTooHighException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+        std::cout << Bobby << std::endl;
+
+        std::cout << "Promoting Mandy's grade by 4:" << std::endl;
+        try {
+            Mandy.incrementGrade(4);
+        } catch (Bureaucrat::GradeTooHighException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+
+        std::cout << Mandy << std::endl;
+        std::cout << "Promoting Tonny's grade by 20:" << std::endl;
+        try {
+            Tonny.incrementGrade(20);
+        } catch (Bureaucrat::GradeTooHighException &ex) {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+        std::cout << Tonny << std::endl;
+
+    } catch (Bureaucrat::GradeException &ex) {
+        std::cout << "Error: " << ex.what() << std::endl;
+    }
+    return 0;
+};
