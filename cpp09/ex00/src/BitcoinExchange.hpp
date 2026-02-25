de359503acf5f27f2b29e7318c0bbacac9a564ab
Andrei Belov <abelov@student.42london.com>
2026-02-25 01:06:33 +0000

    cpp09: Initial commit

--- cpp09/ex00/src/BitcoinExchange.hpp
+++ cpp09/ex00/src/BitcoinExchange.hpp
@@ -6,17 +6,54 @@
 /*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 23:26:33 by abelov            #+#    #+#             */
-/*   Updated: 2025/07/25 23:26:33 by abelov           ###   ########.fr       */
+/*   Updated: 2026/02/25 00:48:59 by abelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <map>
+#include <string>
 
-class BitcoinExchange
-{
+class BitcoinExchange {
+public:
+	BitcoinExchange(void);
+	~BitcoinExchange(void);
+	BitcoinExchange(BitcoinExchange const &src);
 
+	BitcoinExchange &operator=(BitcoinExchange const &src);
+
+	void   load_db(const char *filename);
+	double lookup_value(std::string date, double amount) const;
+	void   print_lookup(std::string date, double amount) const;
+	void   print_lookup(std::string date, long amount) const;
+	void   process_input(const char *filename) const;
+
+	class DBSyntaxException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class FileLoadException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class InvalidDateException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class NegativeAmountException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+private:
+	std::map<std::string, double> _db;
+
+	void _assertDateValid(std::string date) const;
 };
 
 
--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -6,8 +6,27 @@
 /*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 23:26:00 by abelov            #+#    #+#             */
-/*   Updated: 2025/07/25 23:26:01 by abelov           ###   ########.fr       */
+/*   Updated: 2026/02/25 00:49:06 by abelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <iostream>
 
+int	main(int argc, char *argv[])
+{
+	BitcoinExchange	exchange;
+
+	if (argc != 2) {
+		std::cout << "Error: could not open file." << std::endl;
+		return (1);
+	}
+
+	try {
+		exchange.process_input(argv[1]);
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+}
